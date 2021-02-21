dfafa903382a41dbb2985b9fbd1e8ae2b764c322
Alexsandro <alexsandro@alexsandro-Lenovo-G40-70>
2021-02-21 20:42:26 -0300

    add aula12 funcao retorna dados pokemon e url

--- aula12.py
+++ aula12.py
@@ -1,7 +1,6 @@
 import requests
 
 def retorna_dados_cep(cep):
-
     response = requests.get('https://viacep.com.br/ws/{}/json/'.format(cep))
     print(response.status_code)
     print(response.json())
@@ -10,7 +9,20 @@ def retorna_dados_cep(cep):
     print(dados_cep['logradouro'])
     print(dados_cep['complemento'])
     return dados_cep
-    
+
+def retorna_dados_pokemon(pokemon):
+    response = requests.get('https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon))
+    dados_pokemon = response.json()
+    return dados_pokemon
+
+def retorna_response(url):
+    response = requests.get(url)
+    return response.text
+
 if __name__ == '__main__':
-    retorna_dados_cep(22041001)
+    response = retorna_response('https://digitalinnovation.one/')
+    print(response)
+    #retorna_dados_cep(22041001)
+    #dados_pokemon = retorna_dados_pokemon('ditto')
+    #print(dados_pokemon['sprites']['front_shiny'])
 
